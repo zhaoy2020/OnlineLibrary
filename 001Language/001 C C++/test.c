1a388fdb5fdd9019c34b76fddb745ffc90c21a4e
ZhaoY <67251751+zhaoy2020@users.noreply.github.com>
2023-10-25 15:51:04 +0800

    20231025

---
+++ 001Language/001 C C++/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int a, int b){
+    c = a + b;
+    return c;
+}
+
+int main(){
+    printf("hello world! I\' m VSCode\n");
+    int a = 0;
+    int b = 1;
+    printf(add(a, b));
+    return 0;
+}
